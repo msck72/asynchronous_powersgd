b6a42ca0de6e88392d8a2468a9f880947bf076e7
msck72 <chaitanya.mangipudi2002@gmail.com>
2025-03-20 10:22:12 +0530

    modified optimizer_step parameters for compatibility

--- powersgd/__init__.py
+++ powersgd/__init__.py
@@ -4,14 +4,14 @@ from powersgd.powersgd import Aggregator, AllReduce, Config, PowerSGD
 from powersgd.utils import params_in_optimizer
 
 
-def optimizer_step(optimizer: torch.optim.Optimizer, aggregator: Aggregator):
+def optimizer_step(optimizer: torch.optim.Optimizer, aggregator: Aggregator, group, group_id, timer):
     """
     Aggregate gradients across workers using `aggregator`,
     and then take an optimizer step using the aggregated gradient.
     """
     params = params_in_optimizer(optimizer)
     grads = [p.grad.data for p in params]  # type: ignore
-    avg_grads = aggregator.aggregate(grads)  # subtracts the approximation from grads
+    avg_grads = aggregator.aggregate(grads, group, group_id, timer)  # subtracts the approximation from grads
 
     # Temporarily set parameter's gradients to the aggregated values
     for (p, g) in zip(params, avg_grads):
